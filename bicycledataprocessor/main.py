16e06fcd9dc3afa51d2b7a77faa65e04675cc1eb
StefenYin <zqyin@ucdavis.edu>
2012-10-01 15:23:39 -0700

    Deleted the attributes in the task_signals(self) function

--- bicycledataprocessor/main.py
+++ bicycledataprocessor/main.py
@@ -736,8 +736,6 @@ class Run():
         self.compute_rear_wheel_contact_rates()
         self.compute_rear_wheel_contact_points()
         self.compute_front_wheel_contact_points()
-        self.compute_front_wheel_rate()
-        self.compute_front_rear_wheel_contact_forces()
 
         self.topSig = 'task'
 
